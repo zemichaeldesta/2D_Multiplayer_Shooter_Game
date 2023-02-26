5c7ecbf9a6d39e5c48a496bd88368801c8daed0e
mattaniahtsegaye <mattaniah2020@gmail.com>
2023-02-26 11:56:52 -0600

    Jumping player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this code selectes space key to make the player jump.
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+
+        }
+
+      
+    }
+}
