b579dcb34430310a85f4d5aa79d380373bc0e1aa
mattaniahtsegaye <mattaniah2020@gmail.com>
2023-02-26 18:41:11 -0600

    player movement and camera angles

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,25 +1,39 @@
+//writen by Mattaniah Tsegaye
+//feb 26 2023
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-       
+        //this code states the Rigidbody
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //this code selectes space key to make the player jump.
-        if (Input.GetKeyDown("space"))
+        //this code makes the character move horizontaly
+        //it moves the character in the y axis.
+        //GetAxisRaw makes the character move horizontal and the character stops imediatly when you leave the button
+        float direction1 = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(direction1 * 7f, rb.velocity.y);
+
+
+        //this code selectes space button to make the player jump.
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            //this code makes the character move up and down in the x axis.
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
 
         }
 
-      
+
     }
 }
