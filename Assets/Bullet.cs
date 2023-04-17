5fc276774ccb5b989621c11e6808c6237e927e67
zemichaeldesta <kiros.zemic@gmail.com>
2023-04-17 16:51:48 -0500

    Separate Shooting Scene

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-   //bullet speed can easily be changed from unity editor. 
-    
+    //bullet speed can easily be changed from unity editor. 
+
     public float speed;
     private Rigidbody2D rb;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            DestroyObject(gameObject);
+        }
 
-    void Start()//this makes the bulet prefab move
+    }
+
+
+
+        void Start()//this makes the bulet prefab move
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * speed;
